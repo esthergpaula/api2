7ceb2d55bb1378e8c3d3ad996cb8d29d736c37f2
Teh <86411688+esthergpaula@users.noreply.github.com>
2022-10-23 21:05:03 -0300

    Update main.py

--- exercicio 2/main.py
+++ exercicio 2/main.py
@@ -7,13 +7,13 @@ err,con=db.connect()
 reg = db.getLastQuotation(con)
 real = float(input("digite um valor em reais:"))
 if reg!=None:
-    print(f"VALOR EM EURO : {real/float(reg[1])}")
-    print(f"VALOR EM DÓLAR : {real/float(reg[2])}")
+    print(f"valor em euro : {real/float(reg[1])}")
+    print(f"valor em dolar : {real/float(reg[2])}")
 else:
     print("Getting quotation...")
     err,quotation=getCurrenceValueOfMoney(["USD","EUR"])
     db.insertQuotation(con,quotation[0],quotation[1])
-    print(f"VALOR EM EURO : {real/float(quotation[1])}")
-    print(f"VALOR EM DÓLAR : {real/float(quotation[0])}")
+    print(f"valor em euro : {real/float(quotation[1])}")
+    print(f"valor em dolar : {real/float(quotation[0])}")
     
 con.close() 
